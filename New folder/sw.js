421d70794fa6d0873e6ab6cd4181a6edefde67e2
Nate777-Void <nathanclark632@gmail.com>
2026-09-04 22:15:19 -0400

    Delete New folder directory

--- New folder/sw.js
+++
@@ -1,7 +0,0 @@
-<script>
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('./sw.js')
-      .then(() => console.log('VOID Security Protocol: Active'))
-      .catch((err) => console.log('Engine Offline:', err));
-  }
-</script>
